80a5cb6783465458f82a23bf3d6b802d51350ef7
HanaIbrahem <hanaibrahim105@gmail.com>
2023-08-16 10:47:37 +0300

    add all changes to languge test

---
+++ app/Http/Controllers/LanguageController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Redirect;
+
+class LanguageController extends Controller
+{
+    //
+
+
+    public function setLocale($locale)
+    {
+        if (in_array($locale, config('app.locales'))) {
+            session(['locale' => $locale]);
+            App::setLocale($locale);
+        }
+        
+        return Redirect::back();
+    }
+
+    public function changeLanguage(Request $request)
+    {
+        // $validated = $request->validate([
+        //     'language' => 'required',
+        // ]);
+
+        // \Session::put('language', $request->language);
+
+        // return redirect()->back();
+
+        $locale=$request->language;
+        if (in_array($locale, config('app.locales'))) {
+            session(['locale' => $locale]);
+            App::setLocale($locale);
+        }
+
+        return Redirect::back();
+    }
+
+}
